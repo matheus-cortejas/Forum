12897cf978d19b858160b811207e5851c328d63f
matheus-cortejas <euamoremix@gmail.com>
2025-05-27 10:12:53 -0400

    Remoção da opção novos

--- posts/urls.py
+++ posts/urls.py
@@ -4,8 +4,6 @@ from . import views
 urlpatterns = [
     path('threads/', views.threads, name='threads'),  # Página de tópicos com filtros
     path('posts/', views.posts, name='posts'),        # Página de posts com filtros
-    path('threads/novos', views.recent_threads, name='recent_threads'),  # Novos tópicos
-    path('posts/novos', views.recent_posts, name='recent_posts'),        # Novos posts
     path('novidades/', views.ultimas_atividades, name='novidades'),  # Feed de atividades
     path('<str:categoria_slug>/<str:assunto_slug>/thread/<int:thread_id>/', views.thread_detail, name='thread_detail'),
     path('<str:categoria_slug>/<str:assunto_slug>/post/<int:post_id>/', views.post_detail, name='post_detail'),
